2f97419e6f9fff112efdcd483f4e663fb256da2a
Oleksandr Kovalchuk <anxolerd@outlook.com>
2021-03-25 08:05:02 +0200

    Add logging config

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ import logging
 import waitress
 
 from url_shortener.config import Config, load_config
+from url_shortener.logging import configure_logging
 from url_shortener.main import make_app
 
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
+    configure_logging()
     app_config: Config = load_config()
     app = make_app(app_config)
     log.info('Wake up, Samurai! We have an app to build!')
